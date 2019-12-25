c7def03a30eded9b1fc0567684ed9053fd72b064
shakhov <vshakhov@cybertonica.com>
2019-12-25 16:18:35 +0300

    update to api v2.2

--- src/main/java/com/cybertonica/client/AfClient.java
+++ src/main/java/com/cybertonica/client/AfClient.java
@@ -2,13 +2,10 @@ package com.cybertonica.client;
 
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
-import com.eclipsesource.json.JsonValue;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
@@ -23,61 +20,57 @@ public class AfClient {
 
     private HttpURLConnection connection;
     private String url;
-    private String host;
-    private String protocol = "https";
-    private int port;
 
     private AfClient() {}
 
-    private AfClient(String url) throws MalformedURLException {
+    private AfClient(String url) {
         this.url = url;
     }
 
-    private AfClient(String host, int port) throws MalformedURLException {
-        this.host = host;
-        this.port = port;
+    private AfClient(String protocol, String host, int port) {
+        this.url = String.format("%s://%s:%s/api/v2.2/events/", protocol, host, port);
     }
 
-    private AfClient(String protocol, String host, int port) throws MalformedURLException {
-        this.protocol = protocol;
-        this.host = host;
-        this.port = port;
-    }
-
-    public static AfClient get(String url) throws MalformedURLException {
+    public static AfClient get(String url) {
         return new AfClient(url);
     }
 
-    public static AfClient get(String host, int port) throws MalformedURLException {
-        return new AfClient(host, port);
+    public static AfClient get(String host, int port) {
+        return new AfClient("https" ,host, port);
     }
 
-    public static AfClient get(String protocol, String host, int port) throws MalformedURLException {
+    public static AfClient get(String protocol, String host, int port) {
         return new AfClient(protocol, host, port);
     }
 
-    public JsonObject createEvent(AfOptions options) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
-        URL url = this.url != null ? new URL(this.url) : new URL(String.format("%s://%s:%s/api/v2.1/createEvent", protocol, host, port));
-        openConnection(url, options);
-        return postCreate(options.toString());
+    public JsonObject createEvent(AfOptions params) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
+        URL url = new URL(this.url + params.getQuery());
+        openConnection(url, params);
+        System.out.println(url.toString());
+        return postCreate(params.toString());
     }
 
-    public JsonObject updateEvent(AfOptions options) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
-        URL url = this.url != null ? new URL(this.url) : new URL(String.format("%s://%s:%s/api/v2.1/updateEvent", protocol, host, port));
-        openConnection(url, options);
-        return postUpdate(options.toString());
+    public JsonObject updateEvent(AfOptions params) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
+        URL url = new URL(this.url + params.getQuery());
+        System.out.println(url.toString());
+        openConnection(url, params);
+        return postUpdate(params.toString());
     }
 
-    private String sign(String json, String secret) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
-        javax.crypto.spec.SecretKeySpec ks = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA1");
+    public String sign(AfOptions options) throws NoSuchAlgorithmException, InvalidKeyException {
+        return sign(options.toString(), options.signature);
+    }
+
+    private String sign(String json, String secret) throws NoSuchAlgorithmException, InvalidKeyException {
+        javax.crypto.spec.SecretKeySpec ks = new javax.crypto.spec.SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA1");
         javax.crypto.Mac mac = javax.crypto.Mac.getInstance("HmacSHA1");
         mac.init(ks);
-        byte[] result = mac.doFinal(json.getBytes("UTF-8"));
+        byte[] result = mac.doFinal(json.getBytes(StandardCharsets.UTF_8));
         return new sun.misc.BASE64Encoder().encode(result);
     }
 
-    private JsonObject postCreate(String params) throws IOException {
-        byte[] postData = params.getBytes(StandardCharsets.UTF_8);
+    private JsonObject postCreate(String json) throws IOException {
+        byte[] postData = json.getBytes(StandardCharsets.UTF_8);
         connection.setRequestProperty("Content-Length", Integer.toString(postData.length));
         DataOutputStream wr = null;
         try {
@@ -88,14 +81,14 @@ public class AfClient {
                 wr.close();
             }
         }
-        Reader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
+        Reader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
         Scanner s = new Scanner(in).useDelimiter("\\A");
         String jsonStr = s.hasNext() ? s.next() : "";
         return Json.parse(jsonStr).asObject();
     }
 
-    private JsonObject postUpdate(String params) throws IOException {
-        byte[] postData = params.getBytes(StandardCharsets.UTF_8);
+    private JsonObject postUpdate(String json) throws IOException {
+        byte[] postData = json.getBytes(StandardCharsets.UTF_8);
         connection.setRequestProperty("Content-Length", Integer.toString(postData.length));
         DataOutputStream wr = null;
         try {
@@ -106,28 +99,27 @@ public class AfClient {
                 wr.close();
             }
         }
-        Reader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
+        Reader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
         Scanner s = new Scanner(in).useDelimiter("\\A");
         String jsonStr = s.hasNext() ? s.next() : "";
         return new JsonObject().add("", jsonStr);
     }
 
-    private AfClient openConnection(URL url, AfOptions options) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
+    private void openConnection(URL url, AfOptions options) throws IOException, InvalidKeyException, NoSuchAlgorithmException {
         connection = (HttpURLConnection) url.openConnection();
         connection.setDoOutput(true);
         connection.setInstanceFollowRedirects(false);
-        connection.setRequestMethod("POST");
+        connection.setRequestMethod(options.type);
         connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("charset", "UTF-8");
-        connection.setRequestProperty("apiUserId", options.user);
-        connection.setRequestProperty("apiSignature", sign(options.toString(), options.signature));
+        connection.setRequestProperty("charset", StandardCharsets.UTF_8.name());
+        connection.setRequestProperty("X-AF-Team", options.team);
+        connection.setRequestProperty("X-AF-Signature", sign(options));
         connection.setUseCaches(false);
-        return this;
     }
 
-    public static String SHA_256(String src) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public static String SHA_256(String src) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(src.getBytes("UTF-8"));
+        md.update(src.getBytes(StandardCharsets.UTF_8));
         byte[] digest = md.digest();
 
         StringBuilder hexString = new StringBuilder();
